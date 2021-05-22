833d7b19df1fcb5f7f652eff39d733f9063d502a
unifino <unifino@protonmail.com>
2021-05-22 17:26:20 +0300

    db updated

--- src/db/H/Ahadis.ts
+++ src/db/H/Ahadis.ts
@@ -1,6 +1,6 @@
 type db = { a: string, b: string, c: number, d?: string|number }[];
 
-const Kafi = require( "./AL-Kafi.json" ) as db; 
+const Kafi = require( "./Al-Kafi.json" ) as db; 
 const misc = require( "./Misc.json" ) as db; 
 
 export const ahadis = [ ...Kafi, ...misc ];
