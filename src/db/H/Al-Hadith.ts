f93c65a596e99a8d9252e427cc7dd27847a101b2
unifino <unifino@protonmail.com>
2021-06-01 21:57:40 +0300

    ...

--- src/db/H/Al-Hadith.ts
+++ src/db/H/Al-Hadith.ts
@@ -11,5 +11,6 @@ type db = {
 
 const Kafi = require( "./Al-Kafi.json" ) as db; 
 const misc = require( "./Misc.json" ) as db; 
+const mox = require( "./mox.json" ) as db; 
 
-export const Hadith = [ ...Kafi, ...misc ];
+export const Hadith = [ ...mox ];
