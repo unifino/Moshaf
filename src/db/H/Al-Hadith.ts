c0320f3cea59dadd3d4a8925657df35fce2f6ebd
unifino <unifino@protonmail.com>
2021-06-25 23:56:47 +0430

    ...

--- src/db/H/Al-Hadith.ts
+++ src/db/H/Al-Hadith.ts
@@ -4,10 +4,11 @@ const k = require( "./الكافي.json" ) as TS.HDB;
 const v = require( "./وسائل‌الشيعة.json" ) as TS.HDB;
 const n = require( "./نهج‌البلاغة.json" ) as TS.HDB;
 const z = require( "./نهاية.json" ) as TS.HDB;
+const a = require( "./appendix.json" ) as TS.HDB;
 
 // .. temporary collection
 let nHadith: TS.hadithCell[] = [];
 // .. create db based on N
-for ( let p of [ ...k,...v,...n,...z ] ) nHadith[ p.n ] = p;
+for ( let p of [ ...k,...v,...n,...z, ...a ] ) nHadith[ p.n ] = p;
 // .. register it
 export const Hadith = nHadith;
