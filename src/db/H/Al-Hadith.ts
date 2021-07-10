b62e8cd786c94d2b782c9812e9a850a200fb152b
unifino <unifino@protonmail.com>
2021-07-10 19:51:20 +0430

    update DBs

--- src/db/H/Al-Hadith.ts
+++ src/db/H/Al-Hadith.ts
@@ -3,12 +3,13 @@ import * as TS                          from "@/../types/myTypes"
 const k = require( "./الكافي.json" ) as TS.HDB;
 const v = require( "./وسائل‌الشيعة.json" ) as TS.HDB;
 const n = require( "./نهج‌البلاغة.json" ) as TS.HDB;
-const z = require( "./نهاية.json" ) as TS.HDB;
-const a = require( "./appendix.json" ) as TS.HDB;
+const f = require( "./نهج‌الفصاحة.json" ) as TS.HDB;
+const z = require( "./keep.json" ) as TS.HDB;
+const a = require( "./more.json" ) as TS.HDB;
 
 // .. temporary collection
 let nHadith: TS.hadithCell[] = [];
 // .. create db based on N
-for ( let p of [ ...k,...v,...n,...z, ...a ] ) nHadith[ p.n ] = p;
+for ( let p of [ ...k,...v,...n, ...f,...z, ...a ] ) nHadith[ p.n ] = p;
 // .. register it
 export const Hadith = nHadith;
