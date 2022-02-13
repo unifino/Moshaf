bde68b9733681d2fcb67410820311f1975ce7dc4
unifino <unifino@protonmail.com>
2022-02-13 10:56:43 +0300

    new hadith.json (excluded)

--- src/db/H/Al-Hadith.ts
+++ src/db/H/Al-Hadith.ts
@@ -1,16 +1,20 @@
 import * as TS                          from "@/../types/myTypes"
 
-const k = require( "./الكافي.json" ) as TS.HDB;
-const v = require( "./وسائل‌الشيعة.json" ) as TS.HDB;
-const n = require( "./نهج‌البلاغة.json" ) as TS.HDB;
-const f = require( "./نهج‌الفصاحة.json" ) as TS.HDB;
-const m = require( "./مستدرك‌الوسائل.json" ) as TS.HDB;
-const z = require( "./keep.json" ) as TS.HDB;
-const a = require( "./more.json" ) as TS.HDB;
+// const k = require( "./الكافي.json" ) as TS.HDB;
+// const v = require( "./وسائل‌الشيعة.json" ) as TS.HDB;
+// const n = require( "./نهج‌البلاغة.json" ) as TS.HDB;
+// const f = require( "./نهج‌الفصاحة.json" ) as TS.HDB;
+// const m = require( "./مستدرك‌الوسائل.json" ) as TS.HDB;
+// const z = require( "./keep.json" ) as TS.HDB;
+// const a = require( "./more.json" ) as TS.HDB;
 
 // .. temporary collection
 let nHadith: TS.hadithCell[] = [];
-// .. create db based on N
-for ( let p of [ ...k,...v,...n, ...f, ...m, ...z, ...a ] ) nHadith[ p.n ] = p;
+// // .. create db based on N
+// for ( let p of [ ...k,...v,...n, ...f, ...m, ...z, ...a ] ) nHadith[ p.n ] = p;
+
+const a = require( "./hadith.json" ) as TS.HDB;
+nHadith = a;
+
 // .. register it
 export let Hadith = nHadith;
